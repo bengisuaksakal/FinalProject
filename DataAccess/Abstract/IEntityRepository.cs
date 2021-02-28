f318056596880f09758467d117da29d13015f136
bengisu <bengi@DESKTOP-TDLSUTB>
2021-03-01 01:23:07 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // class : referans tip olabilir demek
+    //IEntity: IEntity olabilir yada IEntity implemenete eden bi nesnede olabilir
+    //new() :new'lenebilir olabilir IEntity new'lenemez
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        //filtrelemeyi sağlar
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);// nul filtre vermeyebilirsin o zaman bütün datayı verir
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthWindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //context: Db tabloları ile proje claslarını bağlamak
+    public class NorthWindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {                                 // küçük büyük harf farketmiyor
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
